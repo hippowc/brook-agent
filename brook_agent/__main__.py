c7d107dba542094ada318bd5d902abab92a4d8e5
wangyiduo <wangyiduo@wangyiduodeMacBook-Pro.local>
2026-04-11 15:25:53 +0800

    clear work

--- brook_agent/__main__.py
+++
@@ -1,4 +0,0 @@
-from brook_agent.main import main
-
-if __name__ == "__main__":
-    main()
--- brook_agent/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from brook_agent.tools.registry import run_tool, tool_specs
-
-__all__ = ["run_tool", "tool_specs"]
